3723d3315ba3e9aa46fa0a55044fd869900f72f4
jackanapex <zhutian.wangchn@gmail.com>
2025-08-25 16:42:27 +1000

    add top 10pct primary school zone map

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ import pandas as pd
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def read_school_table(file_name):
+    # read a tab separated file
+    df = pd.read_csv(file_name, sep='\t')
+    # cleanse the school column, by taking only the first part separated by a comma
+    df['School'] = df['School'].apply(lambda x: x.split(', ')[0].upper())
+    # convert df to a dictionary, with the value in School column being the key for each record
+    return df.set_index('School').T.to_dict()
+
 def getCoverage():
     output = {
         'suburb':set(),
@@ -21,6 +29,7 @@ def getCoverage():
         'postCodeRef': {},
         'regionList': set()
     }
+
     try:
         cnx = mysql.connector.connect(user='development', password='flyme2themoon',
                                       host='drivers-dev.letsportal.com.au',
@@ -48,6 +57,16 @@ def getCoverage():
 def getColourSingle(zone_name:str, colour:str):
     # 'YES' - '234851'
     return {zone_name: colour}
+
+def getSchoolColourPlan():
+    # hard code a dict with scores and colour hex codes. scores are only 96, 97, 98, 99 and 100
+    return {
+        100: '#00FF00',
+        99: '#99FF99',
+        98: '#FFFF00',
+        97: '#FF9900',
+        96: '#FF0000'
+    }
 
 def getColourPlan():
     output = {}
@@ -128,13 +147,103 @@ def getLocalBoundariesSaved():
     jsIn = json.load(f)
     return jsIn
 
+def getSchoolBoundariesSaved():
+    f = open('Primary_Integrated_2024.geojson')
+    jsIn = json.load(f)
+    return jsIn
+
 def getLocalBoundaries():
     getURL = 'https://data.gov.au/geoserver/vic-suburb-locality-boundaries-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_af33dd8c_0534_4e18_9245_fc64440f742e&outputFormat=json'
     payload = {}
     headers = {}
     response = requests.request("GET", getURL)
     output = json.loads(response.text)
+    # save this to a local file called locality.json
+    with open('locality.json', 'w') as f:
+        json.dump(output, f)
     return output
+
+def create_school_fig():
+    jsIn = getSchoolBoundariesSaved()
+    suburbDict = read_school_table('top_10pct_primary_schools.tsv')
+    unique_school_names = set(suburbDict.keys())
+    mapboxToken = 'pk.eyJ1IjoiczM3NTA5NTQiLCJhIjoiY2tiZjl3MnZnMGxsdzJxbjRpNDI3a2J3NyJ9.kOhuIXCnopGdLKBVIcvydg'
+    jsCopy = {}
+    layerList = []
+    # colourList = getColourPlan()
+    colourList = getSchoolColourPlan()
+    for score in colourList.keys():
+        jsCopy[score] = copy.deepcopy(jsIn)
+        jsCopy[score]['features'] = []
+    centroid_lat = []
+    centroid_lon = []
+    hoverInfo = []
+    for item in jsIn['features']:
+        if item['properties']['School_Name'].upper() in unique_school_names:
+            # find the first element in suburbDict where the value for key 'School' matches item['properties']['School_Name']
+            matching_element = suburbDict[item['properties']['School_Name'].upper()]
+            if matching_element['State Overall Score'] > 0:
+                jsCopy[matching_element['State Overall Score']]['features'].append(item)
+            centroid_lon.append((max(Extract(item['geometry']['coordinates'][0], 0))
+                                + min(Extract(item['geometry']['coordinates'][0], 0))) / 2)
+            centroid_lat.append((max(Extract(item['geometry']['coordinates'][0], 1))
+                                + min(Extract(item['geometry']['coordinates'][0], 1))) / 2)
+            if item['properties']['School_Name'].upper() != item['properties']['Campus_Name'].upper():
+                hoverInfo.append(item['properties']['School_Name']
+                                + ' '
+                                + item['properties']['Campus_Name'] + '-' + item['properties']['Year_Level']
+                                # choose to show the region or not
+                                + ' : '
+                                + str(matching_element['State Overall Score'])
+                                )
+            else:
+                hoverInfo.append(item['properties']['School_Name']
+                                + '-' + item['properties']['Year_Level']
+                                # choose to show the region or not
+                                + ' : '
+                                + str(matching_element['State Overall Score'])
+                                )
+    for score in colourList.keys():
+        layerList.append(
+            {
+                'source': jsCopy[score],
+                'type': "fill",
+                'below': "traces",
+                'color': colourList[score],
+                'fill': {'outlinecolor':colourList[score]},
+                'opacity': 0.7
+            }
+        )
+    
+    fig = go.Figure(go.Scattermapbox(
+        # choose between marker or text mode
+        mode = "text",
+        lon = centroid_lon, lat = centroid_lat,
+        text = hoverInfo,
+        # hoverinfo = 'text',
+        # marker = {
+        #     'size': 10,
+        #     'color': ["black"],
+        #     'symbol': "information"
+        # }
+    ))
+
+    fig.update_layout(
+        mapbox={
+            'accesstoken': mapboxToken,
+            'style': "light",
+            'center': {'lon': 145.02, 'lat': -37.94},
+            # -37.814364596881546, 144.9597853404137
+            # -37.91725010865954, 145.02890041628314
+            'zoom': 9,
+            'layers': layerList
+        },
+        margin={'l': 0, 'r': 0, 'b': 0, 't': 0}
+    )
+    fig.write_html("index.html")
+    # fig.write_image("teamicare-mel-zone-map.jpg")
+    # fig.show
+    return fig
 
 def createFig():
     jsIn = getLocalBoundariesSaved()
@@ -263,10 +372,11 @@ def createRateCard():
 #     dcc.Graph(figure=figToShow)
 # ])
 if __name__ == '__main__':
-    # getLocalBoundaries()
+    # getLocalBoundariesSaved()
     # getCoverage()
     # createTable()
-    createFig()
+    # createFig()
+    create_school_fig()
     # createRateCard()
     # app.run_server(debug=True)
 
